a03f5c21cc6613bbdc0b54ec2915c09fd93b04ab
nanakura <1749094641@qq.com>
2023-01-23 21:06:53 +0800

    add rpc initializer

---
+++ cmd/video/rpc/user.go
@@ -0,0 +1,61 @@
+package rpc
+
+import (
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/kitex-contrib/obs-opentelemetry/provider"
+	"github.com/kitex-contrib/obs-opentelemetry/tracing"
+	"github.com/kitex-contrib/registry-nacos/resolver"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
+	"mini_tiktok/cmd/user/kitex_gen/userService/userservice"
+	"mini_tiktok/pkg/consts"
+	"mini_tiktok/pkg/mw"
+)
+
+var userService userservice.Client
+
+func initUser() {
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig(consts.NacosAddr, consts.NacosPort),
+	}
+	cc := constant.ClientConfig{
+		NamespaceId:         "public",
+		TimeoutMs:           5000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		LogLevel:            "info",
+		Username:            "nacos",
+		Password:            "nacos",
+	}
+
+	r, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	provider.NewOpenTelemetryProvider(
+		provider.WithServiceName(consts.VideoServiceName),
+		provider.WithExportEndpoint(consts.ExportEndpoint),
+		provider.WithInsecure(),
+	)
+	c, err := userservice.NewClient(
+		consts.UserServiceName,
+		client.WithResolver(resolver.NewNacosResolver(r)),
+		client.WithMuxConnection(1),
+		client.WithMiddleware(mw.CommonMiddleware),
+		client.WithInstanceMW(mw.ClientMiddleware),
+		client.WithSuite(tracing.NewClientSuite()),
+		client.WithClientBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: consts.VideoServiceName}),
+	)
+	if err != nil {
+		panic(err)
+	}
+	userService = c
+}
