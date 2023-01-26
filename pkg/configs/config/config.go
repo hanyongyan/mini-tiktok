b1778907580075d2188fc42a76b0c0bc1a02bdc7
nanakura <1749094641@qq.com>
2023-01-26 16:00:00 +0800

    unmarshal config to struct

---
+++ pkg/configs/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+type GlobalConfig struct {
+	DbConfig struct {
+		Host     string
+		Port     int
+		Username string
+		Password string
+		Database string
+	} `mapstructure:"db"`
+	MqConfig struct {
+		Host string
+		Port int
+	} `mapstructure:"mq"`
+	RedisConfig struct {
+		Host string
+		Port int
+	} `mapstructure:"redis"`
+}
